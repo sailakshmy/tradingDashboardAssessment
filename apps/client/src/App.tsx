569b65f3d67b8a519f839d0dbc4d5d7307d1e2dd
Sai <sailakshmy94@gmail.com>
2026-04-07 21:52:30 +0400

    feat(client): scaffold React TypeScript app with Vite

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div className="hero"></div>
+        <div>
+          <h1>Get started</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
